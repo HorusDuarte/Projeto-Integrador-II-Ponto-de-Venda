a6d4a3f1c794053dac1a690ec44cf146b901b532
Ricardo Duarte <49497130+HorusDuarte@users.noreply.github.com>
2021-08-13 20:35:13 -0300

    Add files via upload

---
+++ SneakersStore/OlaMundo/src/Model/Client.java
@@ -0,0 +1,23 @@
+package Model;
+
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class Client {
+    
+    private int id;
+    private String clientCode;
+    private String clientCPF;
+    private String clientName;
+    private String clientDtBirth; 
+    private String clientEmail;
+    private String clientCellphone;
+    private String clientDtLastBuy;
+    private String clientPostcode;
+    private String clientAddress;
+    private String clientDistrict;
+    private String clientState;
+    private String clientCity;
+
+}
